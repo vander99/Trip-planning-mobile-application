0c2c02ff1f8f0e92bd605e0ae84748748b9ab16d
vander99 <ismail.jemmoudi@gmail.com>
2021-11-28 15:35:40 +0100

    debut projet

--- client/lib/signin.dart
+++ client/lib/signin.dart
@@ -1,4 +1,5 @@
 import 'package:client/authentication_service.dart';
+import 'package:client/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 
@@ -29,7 +30,33 @@ class SignInPage extends StatelessWidget {
               password: passwordController.text.trim());
         },
         child: const Text("Sign in"),
-      )
+      ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(_createRoute());
+        },
+        child: const Text("Go Sign Up"),
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        child: const Text("Discover"),
+      ),
     ]));
   }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (BuildContext context,
+        Animation<double> animation, //
+        Animation<double> secondaryAnimation) {
+      return SignUpPage();
+    },
+    transitionsBuilder: (BuildContext context,
+        Animation<double> animation, //
+        Animation<double> secondaryAnimation,
+        Widget child) {
+      return child;
+    },
+  );
 }
