ad1e71907a2337d5812971dca493e97bcbdbb3a9
vander99 <ismail.jemmoudi@gmail.com>
2021-12-03 19:54:11 +0100

    Ajout de la base de données à l'authentication

--- client/lib/authentication_service.dart
+++ client/lib/authentication_service.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth;
@@ -27,6 +29,12 @@ class AuthenticationService {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
+      CollectionReference users =
+          FirebaseFirestore.instance.collection('users');
+      users.add({
+        'email': email, // John Doe
+        'password': password, // Stokes and Sons
+      });
       return "Signed up";
     } on FirebaseAuthException catch (e) {
       return e.message;
