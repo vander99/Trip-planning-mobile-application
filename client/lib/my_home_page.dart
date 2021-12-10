c7e56622bfe25651c73f1734955e8b4c89d1212f
Anas Quazbary <aquazbary@easycapital.fr>
2021-12-10 10:56:29 +0100

    suite page d'acceuil

--- client/lib/my_home_page.dart
+++ client/lib/my_home_page.dart
@@ -23,8 +23,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MyAppBar(),
-        body: Container(
-          color: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [SearchSection(), HotelSection()],
+          ),
         ));
   }
 }
@@ -53,5 +55,49 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       backgroundColor: Colors.white,
     );
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.grey[200],
+        padding: const EdgeInsets.fromLTRB(10, 25, 10, 10),
+        child: Column(
+          children: [
+            Row(children: [
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.only(left: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 4,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Trouver une ville',
+                            contentPadding: EdgeInsets.all(10),
+                            border: InputBorder.none),
+                      ))),
+              const SizedBox(width: 10),
+              Container(height: 50, width: 100, color: Colors.green)
+            ]),
+            Container(height: 50, color: Colors.red),
+          ],
+        ));
+  }
+}
+
+class HotelSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 1200, color: Colors.white);
   }
 }
