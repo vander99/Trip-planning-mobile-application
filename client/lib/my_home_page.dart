0cb06a4e34ef270aed60ce2d10f441ef217fe3d3
Anas Quazbary <aquazbary@enseirb-matmeca.fr>
2022-01-02 23:53:05 +0100

    fetch page d'acceuil

--- client/lib/my_home_page.dart
+++ client/lib/my_home_page.dart
@@ -1,16 +1,50 @@
+import 'dart:io';
+
 import 'package:client/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 // ignore: constant_identifier_names
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+Future<List> fetchHotel() async {
+  final response = await http.get(
+      Uri.parse(
+          'https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?search_type=HOTEL&name=Bordeaux'),
+      // headers: {
+      //   HttpHeaders.authorizationHeader:
+      //       "8da6724a93msh2894fa97e7a2e57p13d12fjsn61de0eb7ba55",
+      // },
+      headers: {
+        "x-rapidapi-host": "priceline-com-provider.p.rapidapi.com",
+        "x-rapidapi-key": "8da6724a93msh2894fa97e7a2e57p13d12fjsn61de0eb7ba55",
+      });
+
+  if (response.statusCode == 200) {
+    String receivedJson = response.body;
+    List<dynamic> list = json.decode(receivedJson);
+    return list;
+  } else {
+    throw Exception('Failed to load hotel');
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hotels Bookings',
       home: HomePage(),
@@ -18,16 +52,24 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: MyAppBar(),
         body: SingleChildScrollView(
-          child: Column(
-            children: [SearchSection(), HotelSection()],
-          ),
-        ));
+          child: Column(children: <Widget>[SearchSection(), HotelSection()]),
+        ),
+      ),
+    );
   }
 }
 
@@ -62,96 +104,114 @@ class SearchSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.grey[200],
-        padding: const EdgeInsets.fromLTRB(10, 45, 10, 10),
-        child: Column(
-          children: [
-            Row(children: [
-              Expanded(
-                  child: Container(
-                      padding: const EdgeInsets.only(left: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(0, 3))
-                        ],
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            hintText: 'Trouver une ville',
-                            contentPadding: EdgeInsets.all(10),
-                            border: InputBorder.none),
-                      ))),
-              const SizedBox(width: 10),
-              Container(
-                  height: 50,
-                  width: 50,
-                  decoration: const BoxDecoration(boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 4,
-                      offset: Offset(0, 4),
+      color: Colors.grey[200],
+      padding: const EdgeInsets.fromLTRB(10, 45, 10, 10),
+      child: Column(
+        children: [
+          Row(children: [
+            Expanded(
+                child: Container(
+                    padding: const EdgeInsets.only(left: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 4,
+                            offset: Offset(0, 3))
+                      ],
                     ),
-                  ], borderRadius: BorderRadius.all(Radius.circular(25))),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.search,
-                        size: 26,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          shape: const CircleBorder(), primary: Colors.green)))
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: Column(children: const [
-                      Text('Choisir une date',
-                          style: TextStyle(color: Colors.grey)),
-                      Text('12 Dec- 22 Dec')
-                    ])),
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: Column(children: const [
-                      Text('Nombre de personnes',
-                          style: TextStyle(color: Colors.grey)),
-                      Text('3')
-                    ]))
-              ],
-            )
-          ],
-        ));
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Trouver une ville',
+                          contentPadding: EdgeInsets.all(10),
+                          border: InputBorder.none),
+                    ))),
+            const SizedBox(width: 10),
+            Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                  ),
+                ], borderRadius: BorderRadius.all(Radius.circular(25))),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.search,
+                      size: 26,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(), primary: Colors.green)))
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Column(children: const [
+                    Text('Choisir une date',
+                        style: TextStyle(color: Colors.grey)),
+                    Text('12 Dec- 22 Dec')
+                  ])),
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Column(children: const [
+                    Text('Nombre de personnes',
+                        style: TextStyle(color: Colors.grey)),
+                    Text('3')
+                  ]))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
-class HotelSection extends StatelessWidget {
-  final List hotelList = [
-    {
-      'title': 'Hotel1',
-      'place': 'London',
-      'distance': 2,
-      'review': 36,
-      'picture':
-          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-      'price': 180,
-    },
-    {
-      'title': 'Hotel1',
-      'place': 'London',
-      'distance': 2,
-      'review': 36,
-      'picture':
-          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-      'price': 180,
-    }
-  ];
+class HotelSection extends StatefulWidget {
   @override
+  _HotelSectionState createState() => _HotelSectionState();
+}
+
+class _HotelSectionState extends State<HotelSection> {
+  late Future<List> futureHotel;
+
+  @override
+  void initState() {
+    super.initState();
+    futureHotel = fetchHotel();
+  }
+
+  @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     color: Colors.white,
+  //     child: Column(
+  //       children: [
+  //         FutureBuilder<List>(
+  //           future: futureHotel,
+  //           builder: (context, snapshot) {
+  //             if (snapshot.hasData) {
+
+  //               return Text(snapshot.data![1]['itemName']);
+  //             } else if (snapshot.hasError) {
+  //               return Text('${snapshot.error}');
+  //             }
+
+  //             // By default, show a loading spinner.
+  //             return const CircularProgressIndicator();
+  //           },
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(10),
@@ -175,11 +235,33 @@ class HotelSection extends StatelessWidget {
                       ],
                     )
                   ])),
+          // Column(
+          //   children: hotelList.map((hotel) {
+          //     return HotelCard(hotel);
+          //   }).toList(),
+          // ),
           Column(
-            children: hotelList.map((hotel) {
-              return HotelCard(hotel);
-            }).toList(),
-          )
+            children: [
+              FutureBuilder<List>(
+                future: futureHotel,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    // return Text(snapshot.data![1]['itemName']);
+                    return Column(
+                      children: snapshot.data!.map((hotel) {
+                        return HotelCard(hotel);
+                      }).toList(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ],
+          ),
         ]));
   }
 }
@@ -187,56 +269,61 @@ class HotelSection extends StatelessWidget {
 class HotelCard extends StatelessWidget {
   final Map hotelData;
   HotelCard(this.hotelData);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        // child: Image.network(hotelData['picture'])
-        height: 230,
-        margin: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(18),
+      // child: Image.network(hotelData['picture'])
+      height: 230,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(18),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(18),
+                topRight: Radius.circular(18),
+              ),
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade200,
-                spreadRadius: 4,
-                blurRadius: 6,
-                offset: const Offset(0, 3),
-              )
-            ]),
-        child: Column(
-          children: [
-            Container(
-                height: 140,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(18),
-                    topRight: Radius.circular(18),
-                  ),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 5,
-                      child: MaterialButton(
-                          onPressed: () {},
-                          shape: const CircleBorder(),
-                          color: Colors.white,
-                          child: const Icon(
-                            Icons.favorite_outline_rounded,
-                            size: 20,
-                          )),
-                    )
-                  ],
-                ))
-          ],
-        ));
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 5,
+                  child: MaterialButton(
+                      onPressed: () {},
+                      shape: const CircleBorder(),
+                      color: Colors.white,
+                      child: const Icon(
+                        Icons.favorite_outline_rounded,
+                        size: 20,
+                      )),
+                )
+              ],
+            ),
+          ),
+          Text(this.hotelData["itemName"])
+        ],
+      ),
+    );
   }
 }
