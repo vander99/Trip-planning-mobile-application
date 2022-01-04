8bd675623fb1e423570b12cb9f4719b6248355e5
Anas Quazbary <aquazbary@enseirb-matmeca.fr>
2022-01-04 20:22:57 +0100

    barre de recherche fonctionnelle

--- client/lib/my_home_page.dart
+++ client/lib/my_home_page.dart
@@ -36,17 +36,13 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-Future<Hotel> fetchHotel() async {
-  final response = await http
-      .get(Uri.parse('https://api.makcorps.com/free/bordeaux'), headers: {
+Future<Hotel> fetchHotel(String city) async {
+  var url = 'https://api.makcorps.com/free/' + city;
+  final response = await http.get(Uri.parse(url), headers: {
     "Authorization":
-        "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDEyNTM5MjUsImlhdCI6MTY0MTI1MjEyNSwibmJmIjoxNjQxMjUyMTI1LCJpZGVudGl0eSI6MTA1Nn0.pWKlFIEZeYtI-I07wsUZhUCnF393rU3ytDZ9KU-7Y4w"
+        "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDEzMjQ2MjIsImlhdCI6MTY0MTMyMjgyMiwibmJmIjoxNjQxMzIyODIyLCJpZGVudGl0eSI6MTA1Nn0.FGBLwkD-PFYZ9tTK29GhoMzqr00danl4U-HzHMNdaCU"
   });
-
   if (response.statusCode == 200) {
-    // String receivedJson = response.body;
-    // List<dynamic> list = json.decode(receivedJson);
-    // return list;
     return Hotel.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load hotel');
@@ -77,7 +73,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: MyAppBar(text: "Explore"),
       body: SingleChildScrollView(
-        child: Column(children: <Widget>[SearchSection(), HotelSection()]),
+        child: HotelSection(),
       ),
     );
   }
@@ -106,7 +102,7 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
       ),
       title: Text(
         text,
-        style: TextStyle(
+        style: const TextStyle(
           color: Colors.black,
           fontSize: 22,
           fontWeight: FontWeight.w800,
@@ -118,79 +114,6 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 }
 
-class SearchSection extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[200],
-      padding: const EdgeInsets.fromLTRB(10, 45, 10, 10),
-      child: Column(
-        children: [
-          Row(children: [
-            Expanded(
-                child: Container(
-                    padding: const EdgeInsets.only(left: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 4,
-                            offset: Offset(0, 3))
-                      ],
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Trouver une ville',
-                          contentPadding: EdgeInsets.all(10),
-                          border: InputBorder.none),
-                    ))),
-            const SizedBox(width: 10),
-            Container(
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                  ),
-                ], borderRadius: BorderRadius.all(Radius.circular(25))),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(
-                      Icons.search,
-                      size: 26,
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(), primary: Colors.green)))
-          ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  margin: const EdgeInsets.all(10),
-                  child: Column(children: const [
-                    Text('Choisir une date',
-                        style: TextStyle(color: Colors.grey)),
-                    Text('12 Dec- 22 Dec')
-                  ])),
-              Container(
-                  margin: const EdgeInsets.all(10),
-                  child: Column(children: const [
-                    Text('Nombre de personnes',
-                        style: TextStyle(color: Colors.grey)),
-                    Text('3')
-                  ]))
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
 class HotelSection extends StatefulWidget {
   @override
   _HotelSectionState createState() => _HotelSectionState();
@@ -198,66 +121,128 @@ class HotelSection extends StatefulWidget {
 
 class _HotelSectionState extends State<HotelSection> {
   late Future<Hotel> futureHotel;
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    futureHotel = fetchHotel();
+    futureHotel = fetchHotel("bordeaux");
   }
 
   @override
-  // Widget build(BuildContext context) {
-  //   return Container(
-  //     color: Colors.white,
-  //     child: Column(
-  //       children: [
-  //         FutureBuilder<List>(
-  //           future: futureHotel,
-  //           builder: (context, snapshot) {
-  //             if (snapshot.hasData) {
-
-  //               return Text(snapshot.data![1]['itemName']);
-  //             } else if (snapshot.hasError) {
-  //               return Text('${snapshot.error}');
-  //             }
-
-  //             // By default, show a loading spinner.
-  //             return const CircularProgressIndicator();
-  //           },
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
       color: Colors.white,
       child: Column(children: [
         Container(
-            color: Colors.white,
-            height: 50,
-            child: Row(
+          color: Colors.grey[200],
+          padding: const EdgeInsets.fromLTRB(10, 45, 10, 10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 4,
+                              offset: Offset(0, 3))
+                        ],
+                      ),
+                      child: TextField(
+                        controller: myController,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          futureHotel = fetchHotel(myController.text);
+                        });
+                      },
+                      child: const Icon(
+                        Icons.search,
+                        size: 26,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(), primary: Colors.green),
+                    ),
+                  )
+                ],
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text('500 hotels trouvés',
-                      style: TextStyle(color: Colors.black)),
-                  Row(
-                    children: const [
-                      IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.filter_list_outlined,
-                              color: Colors.green, size: 25)),
-                      Text('Filtres', style: TextStyle(color: Colors.black))
-                    ],
+                  Container(
+                      margin: const EdgeInsets.all(10),
+                      child: Column(children: const [
+                        Text('Choisir une date',
+                            style: TextStyle(color: Colors.grey)),
+                        Text('12 Dec- 22 Dec')
+                      ])),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      children: const [
+                        Text('Nombre de personnes',
+                            style: TextStyle(color: Colors.grey)),
+                        Text('3')
+                      ],
+                    ),
                   )
-                ])),
-        // Column(
-        //   children: hotelList.map((hotel) {
-        //     return HotelCard(hotel);
-        //   }).toList(),
-        // ),
+                ],
+              )
+            ],
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('500 hotels trouvés',
+                  style: TextStyle(color: Colors.black)),
+              Row(
+                children: const [
+                  IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.filter_list_outlined,
+                          color: Colors.green, size: 25)),
+                  Text('Filtres', style: TextStyle(color: Colors.black))
+                ],
+              )
+            ],
+          ),
+        ),
         Column(
           children: [
             FutureBuilder<Hotel>(
@@ -265,12 +250,6 @@ class _HotelSectionState extends State<HotelSection> {
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   snapshot.data!.list.removeLast();
-                  // return Text(snapshot.data!.list[0][0]['hotelName']);
-                  var cleanResponse = [];
-                  for (var i = 0; i < snapshot.data!.list.length; i++) {
-                    cleanResponse.add(snapshot.data!.list[i][0]);
-                  }
-                  // return HotelCard(snapshot.data!.list[0]);
                   return Column(
                     children: snapshot.data!.list.map((hotel) {
                       return HotelCard(hotel);
