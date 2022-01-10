24b0e4761591694f3e0404c7e342ca1a1b090740
HamzaEnseirb <Hamza.Mouftih@bordeaux-inp.fr>
2022-01-10 10:11:04 +0100

    UI signin & signup

--- client/lib/signup.dart
+++ client/lib/signup.dart
@@ -14,8 +14,112 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+
     return Scaffold(
-        body: Column(children: [
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        mainAxisAlignment : MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/Icons/planeIcon.png"),
+
+          SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                  labelText: "Email"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                  labelText: "Name"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                  labelText: "Phone number",
+                  hintText: '+91 7894561230',
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                  labelText: "Password"
+                ),
+              ),
+            ),
+
+
+            SizedBox(height: size.height * 0.05),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: RaisedButton(
+                onPressed: () {
+                  context
+                      .read<AuthenticationService>()
+                      .signUp(
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim(),
+                          pseudo: pseudoController.text.trim(),
+                          phone: phoneController.text.trim())
+                      .then((bool signedUp) {
+                    if (signedUp) {
+                      Navigator.of(context).push(homeRoute());
+                    }
+                  });
+                },
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(80.0),
+                    gradient: new LinearGradient(
+                      colors: [
+                        Color(0xFFE8F5E9),
+                        Color(0xFF388E3C)
+                      ]
+                    )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: const Text(
+                    "REGISTER",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+/*
       TextField(
         controller: emailController,
         decoration: const InputDecoration(
@@ -58,6 +162,7 @@ class SignUpPage extends StatelessWidget {
         },
         child: const Text("Create Account"),
       )
+      */
     ]));
   }
 }
